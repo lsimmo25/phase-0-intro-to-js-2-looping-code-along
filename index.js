c7f0562bb0a63853776a1325f9021dbfd05ce61c
Lyle Simmons <lylesimmons125@gmail.com>
2024-01-06 11:17:39 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy bithday to me!`)
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+//wrapGifts(gifts);
+
+function writeCards(name, eventName) {
+    let thankYouCards = [];
+
+    for (let i = 0; i < name.length; i++) {
+        let card = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+
+        thankYouCards.push(card);
+    }
+
+    return thankYouCards
+}
+
+function countDown(x) {
+    let i = 0;
+    while (i <= x) {
+        console.log(x - i);
+        i++;
+        debugger;
+    }
+}
+
+countDown(10);
